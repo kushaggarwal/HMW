8e84bbcd8028cbaad52e7adc8e132346263f1dcd
Sanjeev Aggarwal <sa@Sanjeevs-MacBook-Air.local>
2018-10-27 17:23:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let fname= document.getElementById('fname').value
+let submit = document.getElementById('submit')
+
+
+
+
+var job = ["Banana"];
+document.getElementById("demo").innerHTML = job;
+
+function myFunction() {
+    job.push(fname);
+    document.getElementById("demo").innerHTML = job;
+}
+
+
+var date = document.getElementById("date").value;
+ var lname = document.getElementById("lname").value;
+ var material = document.getElementById("material").value;
+ var time = document.getElementById("time").value;
+ var subject = document.getElementById("subject").value;
+
+
+
+
+ var fruits = [  "Aman" ];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.push(material);
+    document.getElementById("demo").innerHTML = fruits;
+}
+ 
+
+ var fruits = [  "Aman" ];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.push(material);
+    document.getElementById("demo").innerHTML = fruits;
+}
+
+ var fruits = [  "Aman" ];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.push(material);
+    document.getElementById("demo").innerHTML = fruits;
+}
+
+
+
+ var fruits = [  "Aman" ];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.push(material);
+    document.getElementById("demo").innerHTML = fruits;
+}
+
+
+ var fruits = [  "Aman" ];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    fruits.push(material);
+    document.getElementById("demo").innerHTML = fruits;
+}
